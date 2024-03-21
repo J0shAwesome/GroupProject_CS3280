484629e78274675103c95cea62c5bf59c18b268d
J0shAwesome <133062669+J0shAwesome@users.noreply.github.com>
2024-03-21 13:24:45 -0600

    Files for The Project
    This folder contains all the files needed for the CS 3280 Group Project.

---
+++ GroupProject/Items/clsItemLogic.cs
@@ -0,0 +1,8 @@
+﻿namespace GroupProject.Items
+{
+    internal class clsItemLogic
+    {
+        //GetAllItems returns List<clsItem> 
+        // List used to fill up the datagrid in wndItems
+    }
+}
---
+++ GroupProject/Items/wndItems.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace GroupProject.Items
+{
+    /// <summary>
+    /// Interaction logic for wndItems.xaml
+    /// </summary>
+    public partial class wndItems : Window
+    {
+        public wndItems()
+        {
+            InitializeComponent();
+        }
+
+        //bool bHasItemsBeenChanged //Set to true when an item has been added/edited/ Deleted.
+        // Used by main window to know if it needs to refresh items list
+
+        //bool bHasItemsbeenChanged; //Property Mainwindow will look into this window using this property
+    }
+}
